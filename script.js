3264f0f96aa043bc4f3b769d1c7eafc96a81cdf4
Viviana Arias <vivianaarias@MacBook-Air-de-Cristian.local>
2022-07-06 18:51:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let autores = [];
+
+const autor1 = {
+    nombre: "Julio",
+    genero:"drama",
+    pais:"Argentina",
+}
+
+const autor2 = {
+    nombre: "Jane",
+    genero:"romántico",
+    pais:"Inglaterra",   
+}
+
+
+
+
+autores.push(autor1)
+ console.log(autores[0])
